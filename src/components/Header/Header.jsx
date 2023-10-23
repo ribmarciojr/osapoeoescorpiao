17fc0cbb25b023a41afdd42f4333ce93c7945e4e
ribmarciojr <marciojunior2109@gmail.com>
2023-10-23 13:07:09 -0300

    rm: remove "sobre mim" section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
           <ul className='menu-list'>
             <li><Link to='/'>Início</Link></li>
             <li><Link to="/explore">Acervo</Link></li>
-            <li>Sobre mim</li>
+            {/* <li>Sobre mim</li> */}
             <li><Link to='/contact'>Contato</Link></li>
           </ul>
         </nav>
